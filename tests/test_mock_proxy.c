d21e045bd063144d5892e718c04b28c17898d900
Charly Batista <charly@virtlabs.com.br>
2026-05-19 11:53:16 -0300

    fix(tests): use nested #if for __has_feature to fix GCC preprocessor error

--- tests/test_mock_proxy.c
+++ tests/test_mock_proxy.c
@@ -1835,7 +1835,8 @@ static void test_keel_binary_e2e(void)
 {
     TEST_BEGIN("mock proxy: keel binary launched with config pointing to mock backend");
 
-#if defined(__has_feature) && __has_feature(memory_sanitizer)
+#if defined(__has_feature)
+#  if __has_feature(memory_sanitizer)
     /* Under MemorySanitizer the spawned keel binary inherits
      * MSAN_OPTIONS=halt_on_error=1 but has stderr redirected to /dev/null,
      * so any uninstrumented-library call causes a silent crash before keel
@@ -1844,6 +1845,7 @@ static void test_keel_binary_e2e(void)
     printf("  SKIP: keel binary e2e test disabled under MemorySanitizer\n");
     TEST_END();
     return;
+#  endif
 #endif
 
     mock_backend_t *mb = mock_backend_start();
